d1145d55944ed5100b475fd57674f3b774f4036b
공태현 <dev0taehyeon@gmail.com>
2025-06-26 20:45:57 +0900

    [feat] : Redis 설정 및 Token Repository 정의.

--- infra/src/main/java/kong/token/TokenRepository.java
+++ infra/src/main/java/kong/token/TokenRepository.java
@@ -1,4 +1,8 @@
 package kong.token;
 
 public interface TokenRepository {
+    public Long getIdFromToken(String token);
+    public boolean isExist(String token);
+    public void setToken(String token, Long id);
+    public void deleteByToken(String token);
 }
--- infra/src/main/java/kong/token/TokenRepositoryImpl.java
+++ infra/src/main/java/kong/token/TokenRepositoryImpl.java
@@ -1,4 +1,35 @@
 package kong.token;
 
-public class TokenRepositoryImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+@RequiredArgsConstructor
+public class TokenRepositoryImpl implements TokenRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final int EXPIRE_DAY = 1;
+
+    @Override
+    public Long getIdFromToken(String token) {
+        String value = redisTemplate.opsForValue().get(token);
+        return value != null ? Long.parseLong(value) : null;
+    }
+
+    @Override
+    public boolean isExist(String token) {
+        return redisTemplate.opsForValue().get(token) != null;
+    }
+
+    @Override
+    public void setToken(String token, Long id) {
+        redisTemplate.opsForValue().set(token, id.toString(), EXPIRE_DAY, TimeUnit.DAYS);
+    }
+
+    @Override
+    public void deleteByToken(String token) {
+        redisTemplate.delete(token);
+    }
 }
